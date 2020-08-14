d8cfbf6973f8e426ed8435eb597f35ed210571c8
dannyhoover <dofjrtd@gmail.com>
2020-08-13 20:50:06 -0700

    Fully functioning, need to update styling

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,5 +1,5 @@
 // calendar start and end times for the day
-const startTime = 8;
+const startTime = 9;
 const endTime = 17;
 
 // current time
@@ -10,7 +10,7 @@ const dateDisplay = document.querySelector("#currentDay");
 const displayContainer = document.querySelector(".container");
 dateDisplay.textContent = moment().format("dddd, MMMM Do");
 
-// dynamically created rows for events and hour labels with save buttons for each
+// dynamically created hour rows for events and hour labels with save buttons for each
 for (let hour = startTime; hour <= endTime; hour++) {
     let div = document.createElement("div");
     div.className = "row time-block";
@@ -18,7 +18,7 @@ for (let hour = startTime; hour <= endTime; hour++) {
     hourDisplay.className = "hour";
     hourDisplay.textContent = `${hour % 12 || 12} ${hour > 11 ? "PM" : "AM"}`;
     let eventDisplay = document.createElement("textarea");
-    eventDisplay.className = `description ${hour < currentHour ? "past" : hour > currentHour ? "future" : "present"}`;
+    eventDisplay.className = `col-md-10 ${hour < currentHour ? "past" : hour > currentHour ? "future" : "present"}`;
     eventDisplay.value = localStorage.getItem(hour) || ""; 
     let saveButton = document.createElement("button");
     saveButton.className = "saveBtn";
